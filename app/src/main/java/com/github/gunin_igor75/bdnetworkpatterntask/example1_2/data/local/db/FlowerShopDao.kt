c5c2c94ec5ed360f0adc14999f74256d39d7082d
Igor Gunin <gunin-igor75@yandex.ru>
2024-11-23 09:00:01 +0600

    realisation FlowerShopDao
    (cherry picked from commit 783b64aaab45461e58d06e2853df528786a9d9b0)

--- app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/example1_2/data/local/db/FlowerShopDao.kt
+++ app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/example1_2/data/local/db/FlowerShopDao.kt
@@ -1,30 +1,40 @@
 package com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
+import com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity.BouquetDb
 import com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity.BouquetFlowerDb
+import com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity.FlowerBalanceTuple
 import com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity.FlowerDb
+import com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity.WarehouseDb
 
 @Dao
 interface FlowerShopDao {
 
-    @Query("SELECT * FROM flowers")
-    suspend fun getFlowers(): List<FlowerDb>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFlower(vararg flower: FlowerDb)
 
-    @Query("SELECT * FROM bouquet_flower WHERE bouquet_id =:bouquetId")
-    suspend fun getBouquetAndFlowerById(bouquetId: Int): BouquetFlowerDb
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBouquet(vararg bouquet: BouquetDb)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBouquetFlower(vararg bouquetFlower: BouquetFlowerDb)
 
     @Query(
-        "WITH temps  as (SELECT flower_id as id, amount_flower as amount FROM bouquet_flower " +
-                "WHERE bouquet_id =:bouquetId)" +
-                "SELECT (warehouse.amount - temps.amount *:amountBouquet) as count_flower FROM warehouse, temps " +
-                "WHERE temps.id = warehouse.flower_id"
+        "SELECT warehouse.id, " +
+                "(warehouse.amount - bouquet_flower.amount_flower *:amountBouquet) as remainder " +
+                "FROM bouquet_flower " +
+                "INNER JOIN flowers " +
+                "ON bouquet_flower.flower_id = flowers.id " +
+                "INNER JOIN warehouse " +
+                "ON flowers.id = warehouse.flower_id " +
+                "WHERE bouquet_flower.bouquet_id =:bouquetId "
     )
-    suspend fun getBalance(bouquetId: Int, amountBouquet: Int): List<Int>
+    suspend fun getBalance(bouquetId: Int, amountBouquet: Int): List<FlowerBalanceTuple>
 
-    @Query("SELECT * FROM bouquet_flower" +
-        ""
-
-    )
-    suspend fun getBalance1(bouquetId: Int, amountBouquet: Int): List<Int>
+    @Update
+    suspend fun updateFlowerCount(warehouseDb: List<WarehouseDb>)
 }
