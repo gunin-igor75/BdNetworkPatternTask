783b64aaab45461e58d06e2853df528786a9d9b0
Igor Gunin <gunin-igor75@yandex.ru>
2024-11-23 09:00:01 +0600

    realisation FlowerShopDao

--- app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/MainActivity.kt
+++ app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.github.gunin_igor75.bdnetworkpatterntask
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.github.gunin_igor75.bdnetworkpatterntask.app.Repositories
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val apiDao = Repositories.database.getFlowerShopDao()
-        lifecycleScope.launch {
-            val res = apiDao.getFlowers()
-            Log.d("MainActivity", res.toString())
-        }
+
     }
 }
