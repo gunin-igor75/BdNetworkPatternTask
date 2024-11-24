5e0de960b33049abe9a8edcdba374c6c4726ee9f
Igor Gunin <gunin-igor75@yandex.ru>
2024-11-24 17:19:53 +0600

    initial commit

--- app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/example1_2/data/local/entity/BouquetAndFlowerAmountTuple.kt
+++
@@ -1,8 +0,0 @@
-package com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.entity
-
-
-data class BouquetAndFlowerAmountTuple(
-    val id: Int,
-    val name: String,
-    val amount_flower: Int
-)
--- app/src/main/java/com/github/gunin_igor75/bdnetworkpatterntask/example1_2/data/local/repository/FlowerShopRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.github.gunin_igor75.bdnetworkpatterntask.example1_2.data.local.repository
-
-interface FlowerShopRepository {
-    suspend fun buyBouquet(bouquetId: Int, amount: Int): Boolean
-}
